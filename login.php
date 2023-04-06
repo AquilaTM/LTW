1a1be78c33dfbb21b906c26c5e825e71c0dde757
TheMatt3r <128893217+TheMatt3r@users.noreply.github.com>
2023-04-06 16:09:23 +0200

    Add files via upload

---
+++ login.php
@@ -0,0 +1,107 @@
+
+<?php
+require_once('database.php');
+
+if (isset($_POST['register'])) {
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
+    $isUsernameValid = filter_var(
+        $username,
+        FILTER_VALIDATE_REGEXP, [
+            "options" => [
+                "regexp" => "/^[a-z\d_]{3,20}$/i"
+            ]
+        ]
+    );
+    $pwdLenght = mb_strlen($password);
+    
+    if (empty($username) || empty($password)) {
+        $msg = 'Compila tutti i campi %s';
+    } elseif (false === $isUsernameValid) {
+        $msg = 'Lo username non è valido. Sono ammessi solamente caratteri 
+                alfanumerici e l\'underscore. Lunghezza minina 3 caratteri.
+                Lunghezza massima 20 caratteri';
+    } elseif ($pwdLenght < 6 || $pwdLenght > 20) {
+        $msg = 'Lunghezza minima password 6 caratteri.
+                Lunghezza massima 20 caratteri';
+    } else {
+        $password_hash = password_hash($password, PASSWORD_BCRYPT);
+
+        $query = "
+            SELECT id
+            FROM users
+            WHERE username = :username
+        ";
+        
+        $check = $pdo->prepare($query);
+        $check->bindParam(':username', $username, PDO::PARAM_STR);
+        $check->execute();
+        
+        $user = $check->fetchAll(PDO::FETCH_ASSOC);
+        
+        if (count($user) > 0) {
+            $msg = 'Username già in uso %s';
+        } else {
+            $query = "
+                INSERT INTO users
+                VALUES (0, :username, :password)
+            ";
+        
+            $check = $pdo->prepare($query);
+            $check->bindParam(':username', $username, PDO::PARAM_STR);
+            $check->bindParam(':password', $password_hash, PDO::PARAM_STR);
+            $check->execute();
+            
+            if ($check->rowCount() > 0) {
+                $msg = 'Registrazione eseguita con successo';
+            } else {
+                $msg = 'Problemi con l\'inserimento dei dati %s';
+            }
+        }
+    }
+    
+    printf($msg, '<a href="../register.html">torna indietro</a>');
+}
+
+<?php
+session_start();
+require_once('database.php');
+
+if (isset($_SESSION['session_id'])) {
+    header('Location: dashboard.php');
+    exit;
+}
+
+if (isset($_POST['login'])) {
+    $username = $_POST['username'] ?? '';
+    $password = $_POST['password'] ?? '';
+    
+    if (empty($username) || empty($password)) {
+        $msg = 'Inserisci username e password %s';
+    } else {
+        $query = "
+            SELECT username, password
+            FROM users
+            WHERE username = :username
+        ";
+        
+        $check = $pdo->prepare($query);
+        $check->bindParam(':username', $username, PDO::PARAM_STR);
+        $check->execute();
+        
+        $user = $check->fetch(PDO::FETCH_ASSOC);
+        
+        if (!$user || password_verify($password, $user['password']) === false) {
+            $msg = 'Credenziali utente errate %s';
+        } else {
+            session_regenerate_id();
+            $_SESSION['session_id'] = session_id();
+            $_SESSION['session_user'] = $user['username'];
+            
+            header('Location: dashboard.php');
+            exit;
+        }
+    }
+    
+    printf($msg, '<a href="../login.html">torna indietro</a>');
+}
