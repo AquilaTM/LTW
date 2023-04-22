a5014b1e2beafa87982024817ca9bf88d28736db
valerio101 <valeriolubrano11@gmail.com>
2023-04-22 11:16:30 +0200

    22/04/2023 database

--- database.sql
+++ database.sql
@@ -6,3 +6,13 @@ CREATE TABLE utenti(
     data_dinascita DATE NOT NULL,
     password VARCHAR (20) NOT NULL UNIQUE
 );
+
+CREATE TABLE acquista(
+    email VARCHAR(70) NOT NULL UNIQUE,
+    museo1 INT NOT NULL,
+    museo2 INT NOT NULL,
+    museo3 INT NOT NULL,
+    evento1 INT NOT NULL,
+    evento2 INT NOT NULL,
+    evento3 INT NOT NULL
+    )
