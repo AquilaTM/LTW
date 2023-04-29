089b1e91c2cfd74d8c2e1965467e5d27bef81452
AquilaTM <riccardoaquilanti87@gmail.com>
2023-04-29 11:18:22 +0200

    update 29/04

--- index.php
+++ index.php
@@ -106,7 +106,7 @@
 
         <div class="section-title">
           <h2>Eventi</h2>
-          <p>Partecipa ai migliori eventi di Eventi</p>
+          <p>Partecipa ai migliori eventi di Roma</p>
         </div>
 
         <div class="swiper mySwiper" data-aos="fade-up" data-aos-delay="100">
@@ -115,7 +115,7 @@
             <div class="swiper-slide">
               <div class="card">
                 <img src="assets/img/concertoPrimoMaggio.jpg" class="card-img">
-                <div class="card-body">
+                <div class="card-body text-center">
                   <h1>Concerto Primo Maggio</h1>
                 </div>
               </div>
@@ -517,7 +517,7 @@
               <div class="card">
                 <img src="assets/img/villa torlonia.jpg" class="card-img-top">
                 <div class="card-body">
-                  <h1>Villa torlonia</h1>
+                  <h1>Villa Torlonia</h1>
                 </div>
               </div>
             </div>
@@ -599,18 +599,17 @@
   <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
 
   <!-- Vendor JS Files -->
-  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
   <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
   <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
   <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
-  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
   <script src="assets/vendor/php-email-form/validate.js"></script>
 
   <!-- Template Main JS File -->
   <script src="assets/js/main.js"></script>
   <script src="assets/js/login-switch.js"></script>
   <script src="assets/js/label-behaviour.js"></script>
+
+  <script>textFit(document.querySelector("h1"));</script>
 
 </body>
 
